f51d71a86329d6fdeb7d92c65cb3ddb0347d69de
Rodrigo Ferreira <83211924+Rodrrf@users.noreply.github.com>
2023-06-08 19:00:29 +0100

    css todos fdd

--- ultimate-tic-tac-toe/src/App.js
+++ ultimate-tic-tac-toe/src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import TabuleiroUltimate from './components/tabuleiroUltimate/tabuleiroUltimate.component';
-
+import ControlPanel from './components/panelGame/panelGame.component';
 export default function App() {
   
   return (
-    <div className="App">
-      <TabuleiroUltimate/>
+    <div>
+      <div id="container">
+        <div className="main-content"><ControlPanel/></div>
+      </div>
+      <div className='App'>
+       <TabuleiroUltimate/>
+      </div>
+      
     </div>
   );
 }
